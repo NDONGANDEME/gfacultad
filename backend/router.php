a6165a7ece9c4ea41ea2c01697a91c3445c73839
DANA <ndongandeme1994@gmail.com>
2026-01-22 21:14:14 +0100

    Ignorar las carpetas no utiles como nomodule

--- backend/router.php
+++ backend/router.php
@@ -1,13 +1,14 @@
 <?php
-session_start();
-
+//session_start();
+/*
 if (!isset($_SESSION['usuario']) || !isset($_SESSION["rol"])) {
     header("Location: ../index.html");
     exit;
 }
+*/
 
 require_once "utilidades/LimpiarDatos.php";
-header('Content-Type: application/json; charset=utf-8');
+//header('Content-Type: application/json; charset=utf-8');
 
 $ruta = LimpiarDatos::limpiarRuta($_GET['ruta'] ?? '');
 //$accion = LimpiarDatos::limpiarParametro($_GET['accion'] ?? 'index');
@@ -17,19 +18,19 @@ define("BASE_PATH", realpath(__DIR__ . "/../../..") . "/gestionDepartamento/");
 
 switch ($ruta) {
     case 'admin':
-        require_once BASE_PATH . "frontend/admin/controlador/admin.php";
+        require_once BASE_PATH . "admin/index.php";
         break;
 
     case 'secretario':
-        require_once BASE_PATH . "frontend/secretario/controlador/secretario.php";
+        require_once BASE_PATH . "secretario/index.php";
         break;
 
     case 'profesor':
-        require_once BASE_PATH . "frontend/profesor/controlador/profesor.php";
+        require_once BASE_PATH . "profesor/index.php";
         break;
 
     case 'estudiante':
-        require_once BASE_PATH . "frontend/estudiante/controlador/estudiante.php";
+        require_once BASE_PATH . "estudiante/index.php";
         break;
     case 'global':
         require_once BASE_PATH  . "frontend/global/index.php";
@@ -39,3 +40,4 @@ switch ($ruta) {
         header("Location: ../index.html");
         exit;
 }
+
